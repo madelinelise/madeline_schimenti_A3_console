fa367341db7b394cdedfc327db35ad6a55a6507d
Madeline Schimenti <madelinelise@gmail.com>
2015-09-17 20:26:13 -0400

    COP2360 Assignment 3 - Console
    COP2360 Assignment 3 - Console

---
+++ madeline_schimenti_A3_console/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace madeline_schimenti_A3_console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tickets tickets = new Tickets();
+
+            tickets.DisplayMessage();
+
+            tickets.CountTickets();
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ madeline_schimenti_A3_console/Tickets.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace madeline_schimenti_A3_console
+{
+    class Tickets
+    {
+        int counter = 0;
+
+    public void DisplayMessage()
+    {
+        Console.WriteLine("Ticket Ordering System\nEnter the number of tickets to order\nor enter a zero <0> to show the summary\nor enter a negative number when you are done.");
+    }
+
+        public void CountTickets()
+    {
+        
+
+
+        int total;
+        total = 0;
+        Console.Write("Enter number of tickets to order: ");
+        int userInput = Convert.ToInt32(Console.ReadLine());
+               
+       while( userInput >= 0)
+       {
+           total = total + userInput;
+           Console.Write("Enter next order: ");
+           userInput = Convert.ToInt32(Console.ReadLine());
+
+           if (userInput == 0)
+           {
+               Console.WriteLine("Total Tickets Ordered: " + total);
+           }
+          
+       }
+
+       Console.WriteLine("Thank you for your order of " + total + " tickets!");
+    }
+    }
+}
